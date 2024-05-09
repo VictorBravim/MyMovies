dbfb80b8c11caa95eb7e54e056d58e13580a37c1
Victor Bravim <victorrafaelbravim@gmail.com>
2024-05-09 13:40:52 -0300

    Mudança

--- src/components/NavbarMenu.vue
+++ src/components/NavbarMenu.vue
@@ -1,12 +1,12 @@
 <template>
   <nav class="fixed-nav">
     <div class="logo">
-      <img src="caminho_para_o_logo.png" alt="Logo">
+      <img src="@/assets/logo.png" alt="Logo">
     </div>
     <ul>
       <li><a href="#">Início</a></li>
-      <li><a href="#">Filmes Populares</a></li>
-      <li><a href="#">Buscar Filmes</a></li>
+      <li><a href="#">Filmes</a></li>
+      <li><a href="#">Buscar</a></li>
     </ul>
   </nav>
 </template>
@@ -24,15 +24,15 @@ nav {
   left: 0;
   width: 100%;
   background-color: transparent;
-  padding: 10px;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
+  padding-top: 30px;
   align-items: center;
   z-index: 1000;
 }
 
 .logo img {
-  height: 30px; /* Ajuste a altura conforme necessário */
+  height: 30px;
 }
 
 ul {
@@ -40,6 +40,7 @@ ul {
   padding: 0;
   margin: 0;
   display: flex;
+  justify-content: center;
 }
 
 li {
